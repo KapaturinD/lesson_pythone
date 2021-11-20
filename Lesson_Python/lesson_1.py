ee41c6e08caebbaa13983511f0ccbd904adaa509
Denis <60378291+KapaturinD@users.noreply.github.com>
2021-11-21 02:40:31 +0600

    Delete Lesson_Python directory

--- Lesson_Python/lesson_1.py
+++
@@ -1,9 +0,0 @@
-test_1 = 5
-test_2 = 2
-print("Переменные test_1 и test_2 - ", test_1, test_2)
-
-string_1 = input("Введите первую строку: ")
-number_1 = int(input("Введите первое число: "))
-string_2 = input("Введите вторую строку: ")
-number_2 = int(input("Введите второе число: "))
-print("Введеные значения -", string_1, number_1, string_2, number_2)
--- Lesson_Python/lesson_2.py
+++
@@ -1,5 +0,0 @@
-user = int(input("Введите время в секундах: "))
-hours = user // 3600
-minutes = (user - hours * 3600) // 60
-seconds = user - (hours * 3600 + minutes * 60)
-print(f"Время  {hours:02} : {minutes:02} : {seconds:02}")
